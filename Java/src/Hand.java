91389478e92193a0bf85eb57cfcae838a37eda32
sangh <sangh@10.48.25.158>
2026-02-10 17:44:14 -0500

    add new class hand

---
+++ Java/src/Hand.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Milandeep singh
+ * @author jaskaran singh gill
+ * @author inderpartap singh
+ * @author mohan singh
+ * 
+ */
+public class Hand {
+    
+}
